019a7c7beb4fa56bf2da3a27ba06d8e40d413d05
Kvvnn <hermano1uno@gmail.com>
2025-10-05 00:01:23 +0900

    Google AI tst

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello, world")
+import { GoogleGenAI } from "@google/genai";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+    /*
+    config: {
+      thinkingConfig: {
+        thinkingBudget: 0, // Disables thinking
+      },
+    }
+    */
+  });
+  console.log("Gemini saids: " + response.text);
+}
+
+await main();
+
+console.log("hello, world!")

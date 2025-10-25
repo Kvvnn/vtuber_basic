db8cf05fa4bffc945fc9e970c8ebf0be21b8ddec
kvvnn <hermano1uno@gmail.com>
2025-10-25 16:12:09 +0900

    merge jules

--- src/controller/globalController.js
+++ src/controller/globalController.js
@@ -21,8 +21,6 @@ let videos = [
 
 import AIResponse from "./AI/gemini.js"
 
-// const AIResponse = "Hi, I'm AI!"
-
 export const home = (req, res, next) => {
     res.render('home',{videos,AIResponse})
 }

46101310d89b91abdc50c0ef88ec34f7606032b2
kvvnn <hermano1uno@gmail.com>
2025-10-25 14:19:42 +0900

    update mixins and DB examples

--- src/controller/globalController.js
+++ src/controller/globalController.js
@@ -1,9 +1,30 @@
+let videos = [
+    {
+        title:"first",
+        description:"Hi",
+        rating:"1"
+    },{
+        title:"second",
+        description:"2nd",
+        rating:"2"
+    },{
+        title:"third",
+        description:"3rd",
+        rating:"3"
+    },{
+        title:"fourth",
+        description:"4th",
+        rating:"4"
+    }
+
+]
+
 import AIResponse from "./AI/gemini.js"
 
 // const AIResponse = "Hi, I'm AI!"
 
 export const home = (req, res, next) => {
-    res.render('home',{AIResponse})
+    res.render('home',{videos,AIResponse})
 }
 
 export const live2d = (req, res) => {
